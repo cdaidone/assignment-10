bd577ca9ca99f202167d5038ab6bdc1290bb24bf
cdaidone <cdaidone@ufl.edu>
2015-11-06 21:19:23 -0500

    First commit

---
+++ js/bootstrap_markup.js
@@ -0,0 +1,48 @@
+<div id="carousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel" data-slide-to="1"></li>
+    <li data-target="#carousel" data-slide-to="2"></li>
+    <li data-target="#carousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="images/dog1.jpg" alt="...">
+      <div class="carousel-caption">
+        <p>President Obama runs with Bo through the White House.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/dog2.jpg" alt="...">
+      <div class="carousel-caption">
+        <p>"President Obama plays with Bo on the front lawn of the White House."</p>
+      </div>
+    </div>
+    ...
+    <div class="item">
+    	<img src="images/dog3.jpg" alt="...">
+    	<div class="carousel-caption">
+    	<p>President Obama plays chase with Bo on the front lawn of the White House."</p>    	
+    	</div>
+    </div>
+    <div class="item">
+    	<img src="images/dog4.jpg" alt="...">
+    	<div class="carousel-caption">
+    	<p>Bo greats the President.</p>
+    	</div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
